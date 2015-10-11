71dd57f953a8dd319cddd57e3599dc21231ccabd
Andrew Porter <mephisto83@hotmail.com>
2015-10-11 15:43:08 +0200

    as dfasdf
    asdf asd

---
+++ dev-harness/presentation-blender/blend/presentation/template/ExtraInfo.js
@@ -0,0 +1,5 @@
+﻿MEPH.define('Blend.presentation.template.ExtraInfo', {
+    alias: 'blend_presentation_template_extrainfo',
+    templates: true,
+    extend: 'MEPH.control.Control' 
+});
---
+++ dev-harness/presentation-blender/blend/presentation/template/MidiFile.js
@@ -0,0 +1,6 @@
+﻿MEPH.define('Blend.presentation.template.MidiFile', {
+    alias: 'blend_presentation_template_midifile',
+    templates: true,
+    requires: ['Blend.presentation.template.ExtraInfo'], 
+    extend: 'MEPH.control.Control'
+});

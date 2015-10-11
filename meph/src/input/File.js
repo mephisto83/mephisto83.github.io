6bbeb5de7fe3077a95db59feb3cf03e1b5f5fb19
Andrew Porter <mephisto83@hotmail.com>
2015-10-11 15:45:01 +0200

    a sdjfaklsdjfkl
    kjlaskdf jlasd

---
+++ meph/src/input/File.js
@@ -0,0 +1,36 @@
+﻿
+MEPH.define('MEPH.input.File', {
+    alias: 'file',
+    templates: true,
+    extend: 'MEPH.field.FormField',
+    properties: {
+        type: 'file',
+        step: null,
+        disabled: null,
+        max: null,
+        maxlength: null,
+        min: null,
+        pattern: null,
+        readonly: null,
+        required: null,
+        placeholder: null, 
+        size: null
+    },
+    initialize: function () {
+        var me = this,
+            properties = MEPH.Array(['value']);
+
+        me.callParent.apply(me, arguments);
+        properties.foreach(function (prop) {
+            me.addTransferableAttribute(prop, {
+                object: me,
+                path: prop
+            });
+        });
+    },
+    onLoaded: function () {
+        var me = this;
+        me.callParent.apply(me, arguments);
+        me.fire('altered', { path: 'type', references: [] });
+    }
+});
